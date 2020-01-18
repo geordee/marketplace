d89a520a2f99a571fbabfe091ca791f8a0e1b872
Geordee Naliyath <geordee@gmail.com>
2020-01-19 00:16:52 +0400

    List Products in Marketplace

--- test/contracts/Markeplace.test.js
+++ test/contracts/Markeplace.test.js
@@ -1,6 +1,10 @@
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
 const Marketplace = artifacts.require('./Marketplace.sol')
 
-contract('Marketplace', (accounts) => {
+contract('Marketplace', ([deployer, seller, buyer]) => {
   let marketplace
 
   before(async () => {
@@ -19,6 +23,31 @@ contract('Marketplace', (accounts) => {
     it('has a name', async() => {
       const name = await marketplace.name()
       assert.equal(name, 'Marketplace')
+    })
+  })
+
+  describe('products', async () => {
+    let result, productCount
+
+    before(async () => {
+      result = await marketplace.createProduct('Leather Wallet', web3.utils.toWei('1', 'Ether'), { from: seller })
+      productCount = await marketplace.productCount()
+    })
+
+    it('creates products', async() => {
+      assert.equal(productCount, 1)
+      // console.log(result.logs)
+      const event = result.logs[0].args;
+      assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+      assert.equal(event.name, 'Leather Wallet', 'name is correct')
+      assert.equal(event.price, web3.utils.toWei('1', 'Ether'), 'price is correct')
+      assert.equal(event.owner, seller, 'owner  is correct')
+      assert.equal(event.purchased, false, 'purchased is correct')
+    })
+
+    it('does not create invalid products', async() => {
+      await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected
+      await marketplace.createProduct('Leather Wallet', 0, { from: seller }).should.be.rejected
     })
 
   })
